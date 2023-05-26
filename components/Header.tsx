d718dca3787fbdc0588cef4256c514fb78bf4925
Rokas <rookas.rudzenskas@gmail.com>
2023-05-26 17:23:22 +0300

    🎨 style(Header.tsx): add header image to improve UI 🔧 chore(next.config.js): add domains to next/image configuration to allow images from external sources to be displayed

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import Image from "next/image";
 
 const Header = ({}) => {
   return (
-    <div>
-      
-    </div>
+    <header>
+      <Image
+        src="https://links.papareact.com/c2cdd5"
+        alt={"header"}
+        width={300}
+        height={100}
+        className="w-44 md:w-56 pb-10 md:pb-0 object-contain cursor-pointer"
+      />
+    </header>
   );
 };
 
--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['images.unsplash.com', 'links.papareact.com'],
+  }
+}
 
 module.exports = nextConfig
