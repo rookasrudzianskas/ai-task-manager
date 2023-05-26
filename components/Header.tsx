af6c10bb20bcf85a125b3949330066a0d57db555
Rokas <rookas.rudzenskas@gmail.com>
2023-05-26 20:14:43 +0300

    🚀 feat(Header.tsx): add search functionality to search todos by title 🚀 feat(BoardStore.ts): add searchString and setSearchString to store search query string

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,10 @@ import React from 'react';
 import Image from "next/image";
 import {MagnifyingGlassIcon, UserCircleIcon} from "@heroicons/react/24/solid";
 import Avatar from 'react-avatar';
+import {useBoardStore} from "@/store/BoardStore";
 
 const Header = ({}) => {
+  const [searchString, setSearchString] = useBoardStore((state) => [state.searchString, state.setSearchString]);
   return (
     <header>
       <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
@@ -19,7 +21,7 @@ const Header = ({}) => {
         <div className="flex items-center space-x-5 flex-1 justify-end w-full">
           <form className="flex items-center space-x-5 bg-white rounded-md p-3 shadow-md flex-1 md:flex-initial">
             <MagnifyingGlassIcon className="h-6 w-6 text-gray-400"/>
-            <input type="text" className="flex-1 outline-none p-2" placeholder={'Search'}/>
+            <input type="text" value={searchString} onChange={(e) => setSearchString(e.target.value)} className="flex-1 outline-none p-2" placeholder={'Search'}/>
             <button type={'submit'} hidden>Search</button>
           </form>
           {/*@ts-ignore*/}
