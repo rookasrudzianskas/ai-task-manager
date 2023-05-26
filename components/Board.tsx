39d4125c900ba0a6239a8a0cc4eb5eae1725fa37
Rokas <rookas.rudzenskas@gmail.com>
2023-05-26 19:51:36 +0300

    🚀 feat(Board.tsx): add support for dragging tasks between columns

--- components/Board.tsx
+++ components/Board.tsx
@@ -30,6 +30,40 @@ const Board = ({}) => {
     const columns = Array.from(board.columns)
     const startColIndex = columns[Number(source.droppableId)];
     const finishColIndex = columns[Number(destination.droppableId)];
+
+    const startCol: Column = {
+      id: startColIndex[0],
+      todos: startColIndex[1].todos
+    }
+
+    const finishCol: Column = {
+      id: finishColIndex[0],
+      todos: finishColIndex[1].todos
+    }
+
+    if(!startCol || !finishCol) return;
+    if(source.index === destination.index && startCol === finishCol) return;
+
+    const newTodos = startCol.todos
+    const [todoMoved] = newTodos.splice(source.index, 1);
+
+    if(startCol.id === finishCol.id) {
+      // same column task drag
+      newTodos.splice(destination.index, 0, todoMoved);
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos
+      };
+
+      const newColumns = new Map(board.columns);
+      newColumns.set(startCol.id, newCol);
+
+      setBoardState({
+        ...board, columns: newColumns
+      })
+    } else {
+      // different column task drag
+    }
   }
 
   return (
