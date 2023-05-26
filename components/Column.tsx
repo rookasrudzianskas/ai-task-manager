be6f79925bca92451f0f72c86850a91e294b9fa4
Rokas <rookas.rudzenskas@gmail.com>
2023-05-26 19:18:07 +0300

    🎨 style(Column.tsx): add a droppable area to the column component to enable drag and drop functionality 🐛 fix(Column.tsx): pass draggableId and index props to Draggable component to avoid console errors

--- components/Column.tsx
+++ components/Column.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Draggable} from "react-beautiful-dnd";
+import {Draggable, Droppable} from "react-beautiful-dnd";
 
 type Props = {
   id: TypedColumn;
@@ -9,14 +9,24 @@ type Props = {
 
 const Column = ({id, todos, index}: Props) => {
   return (
-    <Draggable>
+    <Draggable draggableId={id} index={index}>
       {(provided) => (
         <div
           {...provided.draggableProps}
           {...provided.dragHandleProps}
           ref={provided.innerRef}
         >
-
+          <Droppable droppableId={index.toString()} type="card">
+            {(provided, snapshot) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                className={`p-2 rounded-2xl shadow-sm ${snapshot.isDraggingOver ? 'bg-green-200' : "bg-white/50"}`}
+              >
+                <h2>{id}</h2>
+              </div>
+            )}
+          </Droppable>
         </div>
       )}
     </Draggable>
