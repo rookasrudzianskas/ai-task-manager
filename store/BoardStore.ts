4feab3951c6c1a95e63fee6b281ba72ea1aa8dea
Rokas <rookas.rudzenskas@gmail.com>
2023-05-26 20:00:34 +0300

    🚀 feat(BoardStore.ts): add updateTodoInDB function to update a todo's status in the database
    ℹ️ The updateTodoInDB function uses the Appwrite SDK to update a todo's status in the database. It takes in a todo object and a columnId, and updates the todo's status field in the database to the given columnId.

--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,10 +1,12 @@
 import { create } from 'zustand'
 import {getTodosGroupedByColumn} from "@/lib/getTodosGroupedByColumn";
+import {databases} from "@/appwrite";
 
 interface BoardState {
   board: Board;
   getBoard: () => void;
   setBoardState: (board: Board) => void;
+  updateTodoInDB: (todo: Todo, columnId: TypedColumn) => void;
 }
 
 export const useBoardStore = create<BoardState>((set) => ({
@@ -16,4 +18,16 @@ export const useBoardStore = create<BoardState>((set) => ({
     set({ board });
   },
   setBoardState: (board) => set({ board }),
+
+  updateTodoInDB: async (todo, columnId) => {
+    await databases.updateDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,
+      todo.$id,
+      {
+        title: todo.title,
+        status: columnId
+      }
+    )
+  }
 }))
