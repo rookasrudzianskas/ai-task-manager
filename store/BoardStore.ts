26e4e3179dc7ede3b88c576346d93f719287ab84
Rokas <rookas.rudzenskas@gmail.com>
2023-05-26 21:19:06 +0300

    🐛 fix(BoardStore.ts): import storage from appwrite to use it in deleteTask function ✨ feat(BoardStore.ts): add deleteTask function to delete a task from a todo and delete its image from storage and document from database.

--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand'
 import {getTodosGroupedByColumn} from "@/lib/getTodosGroupedByColumn";
-import {databases} from "@/appwrite";
+import {databases, storage} from "@/appwrite";
 
 interface BoardState {
   board: Board;
@@ -9,9 +9,10 @@ interface BoardState {
   updateTodoInDB: (todo: Todo, columnId: TypedColumn) => void;
   searchString: string;
   setSearchString: (searchString: string) => void;
+  deleteTask: (taskIndex: number, todoId: Todo, id: TypedColumn) => void;
 }
 
-export const useBoardStore = create<BoardState>((set) => ({
+export const useBoardStore = create<BoardState>((set, get) => ({
   board: {
     columns: new Map<TypedColumn, Column>()
   },
@@ -33,5 +34,22 @@ export const useBoardStore = create<BoardState>((set) => ({
         status: columnId
       }
     )
+  },
+  deleteTask: async (taskIndex: number, todo: Todo, id: TypedColumn) => {
+    const newColumns = new Map(get().board.columns);
+
+    // delete todoId from new Columns
+    newColumns.get(id)?.todos.splice(taskIndex, 1);
+    set({ board: { columns: newColumns } });
+
+    if(todo.image) {
+      await storage.deleteFile(todo.image.bucketId, todo.image.fileId);
+    }
+
+    await databases.deleteDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,
+      todo.$id
+    );
   }
 }))
