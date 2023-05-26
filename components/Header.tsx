73ec969a8aea0565d21c49964cccc38892bb3e88
Rokas <rookas.rudzenskas@gmail.com>
2023-05-26 17:51:13 +0300

    🎨 style(Header.tsx): add gradient background to header for better visual appeal and user experience

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,6 +8,7 @@ const Header = ({}) => {
   return (
     <header>
       <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
+        <div className="absolute top-0 left-0 w-full h-96 bg-gradient-to-br from-pink-400 to-[#0055D1] rounded-md filter blur-3xl opacity-50 -z-50"></div>
         <Image
           src="https://links.papareact.com/c2cdd5"
           alt={"header"}
