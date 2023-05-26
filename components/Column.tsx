f7929daa0a13fff97781d19c7f80d93c4918dd04
Rokas <rookas.rudzenskas@gmail.com>
2023-05-26 19:21:15 +0300

    🎨 style(Column.tsx): add column text mapping object to improve semantics and readability ✨ feat(Column.tsx): add todo count badge to column header to improve user experience and provide more information

--- components/Column.tsx
+++ components/Column.tsx
@@ -7,6 +7,14 @@ type Props = {
   index: number;
 }
 
+const idToColumnText: {
+  [key in TypedColumn]: string;
+} = {
+  "todo": "To Do",
+  "inprogress": "In Progress",
+  "done": "Done"
+}
+
 const Column = ({id, todos, index}: Props) => {
   return (
     <Draggable draggableId={id} index={index}>
@@ -23,7 +31,11 @@ const Column = ({id, todos, index}: Props) => {
                 ref={provided.innerRef}
                 className={`p-2 rounded-2xl shadow-sm ${snapshot.isDraggingOver ? 'bg-green-200' : "bg-white/50"}`}
               >
-                <h2>{id}</h2>
+                <h2 className="flex items-center justify-between font-bold text-xl">
+                  {idToColumnText[id]}
+                  <span className="text-gray-500 bg-gray-200 rounded-full px-2 py-2 font-normal text-sm">{todos.length}</span>
+                </h2>
+
               </div>
             )}
           </Droppable>
