0b648c38b3ccc42179dcc88cb5e18a66d8cd7f19
Rokas <rookas.rudzenskas@gmail.com>
2023-05-26 20:33:17 +0300

    🚀 feat(route.ts): add openai chat completion API call to generate a summary of todos and send it to the user.

--- app/api/generateSummary/route.ts
+++ app/api/generateSummary/route.ts
@@ -1,8 +1,26 @@
 import {Request} from "next/dist/compiled/@edge-runtime/primitives/fetch";
+import openai from "@/openai";
 
 export async function POST(request: Request) {
   const { todos } = await request.json();
   console.log(todos);
 
   // Connect with openai
+
+  const response = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    temperature: 0.8,
+    n: 1,
+    stream: false,
+    messages: [
+      {
+        role: "system",
+        content: `When responding, welcome user always as Mr. Rokas and say welcome to the byRookas App! Limit the response to 200 characters.`
+      },
+      {
+        role: "user",
+        content: `Hi there, provide a summary of the following todos. Count how many todos are in each category such as To do, in progress and done, then tell the user to have a productive day! Here's the data: ${JSON.stringify(todos)}`,
+      },
+    ]
+  })
 }
