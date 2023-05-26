723c7c94dbad83f043d938f0da2e33b6a8748cd5
Rokas <rookas.rudzenskas@gmail.com>
2023-05-26 17:19:24 +0300

    🎨 style(page.tsx): add Header component to improve code organization and readability

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import Header from "@/components/Header"
 
 export default function Home() {
   return (
     <main>
-      {/*  Header   */}
+      <Header />
       {/*  Board  */}
     </main>
   )
