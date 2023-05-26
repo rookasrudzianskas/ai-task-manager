df164dff85fde46e81a77e340d40037edab44ecc
Rokas <rookas.rudzenskas@gmail.com>
2023-05-26 17:40:09 +0300

    💄 style(Header.tsx): add a new section to the header with a message about the app's functionality and a call to action

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,7 +15,7 @@ const Header = ({}) => {
           height={100}
           className="w-44 md:w-56 pb-10 md:pb-0 object-contain cursor-pointer"
         />
-        <div className="flex">
+        <div className="flex items-center space-x-5 flex-1 justify-end w-full">
           <form className="flex items-center space-x-5 bg-white rounded-md p-3 shadow-md flex-1 md:flex-initial">
             <MagnifyingGlassIcon className="h-6 w-6 text-gray-400"/>
             <input type="text" className="flex-1 outline-none p-2" placeholder={'Search'}/>
@@ -24,6 +24,12 @@ const Header = ({}) => {
           {/*@ts-ignore*/}
           <Avatar name="Rokas Rudzianskas" round color="#0055D1" size={'50'} />
         </div>
+      </div>
+      <div className="flex items-center justify-center px-5 md:py-5">
+        <p className="flex items-center text-sm font-light pr-5 rounded-xl w-fit shadow-xl bg-white italic max-w-3xl text-[#0055D1] p-5">
+          <UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] mr-1" />
+          GPT is summarizing the text for you in a few seconds and you can use it for free. Just sign up and start using it.
+        </p>
       </div>
     </header>
   );
