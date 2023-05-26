56453086ddf97bc8f6e98c79dc4ad74f5d0233d4
Rokas <rookas.rudzenskas@gmail.com>
2023-05-26 21:24:27 +0300

    🎨 style(Modal.tsx): format code and add missing imports ✨ feat(Modal.tsx): add modal component with headlessui/react library to improve user experience

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,12 +1,51 @@
-import React from 'react';
+"use client";
+import { useState, Fragment } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
 
-const Modal = ({}) => {
+function Modal() {
+  let [isOpen, setIsOpen] = useState(true)
+
   return (
-    <div>
-      
-    </div>
-  );
-};
+    // Use the `Transition` component at the root level
+    <Transition show={isOpen} as={Fragment}>
+      <Dialog onClose={() => setIsOpen(false)}>
+        {/*
+          Use one Transition.Child to apply one transition to the backdrop...
+        */}
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black/30" />
+        </Transition.Child>
 
-export default Modal;
-// by Rokas with ❤️
+        {/*
+          ...and another Transition.Child to apply a separate transition
+          to the contents.
+        */}
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          <Dialog.Panel>
+            <Dialog.Title>Deactivate account</Dialog.Title>
+
+            {/* ... */}
+          </Dialog.Panel>
+        </Transition.Child>
+      </Dialog>
+    </Transition>
+  )
+}
+
+export default Modal
