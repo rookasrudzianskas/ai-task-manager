ac3ccb0d828e3fa4044a9e0051eaaf007594bba0
Rokas <rookas.rudzenskas@gmail.com>
2023-05-27 13:12:49 +0300

    🔨 refactor(uploadImage.ts): rename image parameter to file to improve semantics ✨ feat(uploadImage.ts): add validation to check if file exists before uploading it to storage ✨ feat(uploadImage.ts): add return statement to return the uploaded file after it's uploaded to storage

--- lib/uploadImage.ts
+++ lib/uploadImage.ts
@@ -1,3 +1,13 @@
-export const uploadImage = async (image: File) => {
+import {ID, storage} from "@/appwrite";
 
+export const uploadImage = async (file: File) => {
+  if(!file) return;
+
+  const fileUploaded = await storage.createFile(
+    "6470cc02534ebadaa097",
+    ID.unique(),
+    file
+  )
+
+  return fileUploaded;
 }
