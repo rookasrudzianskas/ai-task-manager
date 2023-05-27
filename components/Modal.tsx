9bbdc9494467213a1c2ce772cebdbeec2cadfea4
Rokas <rookas.rudzenskas@gmail.com>
2023-05-27 12:15:41 +0300

    ✨ feat(Modal.tsx): add Dialog.Panel and Dialog.Title components to improve accessibility and semantics of the modal window

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -32,7 +32,11 @@ function Modal() {
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <div className="fixed inset-0 bg-black bg-opacity-25" />
+              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900 pb-2">
+                  Add a task
+                </Dialog.Title>
+              </Dialog.Panel>
             </Transition.Child>
           </div>
         </div>
